7f1604af9644710fd04b48c749634bf6afc2ceca
Joe Booth <joe@joebooth.com>
2020-06-19 11:18:30 -0700

    basic game board

---
+++ UnityConnectAlpha/Assets/Scripts/GameBoard.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameBoard : MonoBehaviour
+{
+    public int[] CellStates;
+    public GameObject Cell;
+
+    List<List<GameObject>> _cells;
+    public int Size;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void InitializeBoard(int size)
+    {
+        CellStates = Enumerable.Range(0, size*size).Select(x=>0).ToArray();
+        _cells = new List<List<GameObject>>();
+        Vector3 position = this.transform.position;
+        position.x -= ((float)size-1) / 2f;
+        position.y = 0f;
+        position.z += ((float)size-1) / 2f;
+        for (int x = 0; x < size; x++)
+        {
+            var row = new List<GameObject>();
+            for (int z = 0; z < size; z++)
+            {
+                var cell = GameObject.Instantiate(Cell, position, this.transform.rotation);
+                cell.transform.parent = this.transform;
+                position.x += 1f;
+                row.Add(cell);
+            }
+            position.x -= (float)size;
+            position.z -= 1f;
+            _cells.Add(row);
+        }
+    }
+}
---
+++ UnityConnectAlpha/Assets/Scripts/TicTacAgent.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using SpawnableEnvs;
+
+public class TicTacAgent : Agent
+{
+    // Start is called before the first frame update
+
+    [Header("Settings")]
+    [Tooltip("The number of rows and columns in the grid.")]
+    public int Size = 3;
+    [Tooltip("How many units in a row are needed to win the game.")]
+    public int WinCount = 3;
+    [Tooltip("Pie rule allows the 2nd player to take the position of the 1st players 1st go")]
+    public bool PieRule = false;
+
+
+    SpawnableEnv _spawnableEnv;
+    GameBoard _gameBoard;
+
+
+    override public void Initialize() 
+    {
+        // grab access to objects
+        _spawnableEnv = GetComponentInParent<SpawnableEnv>();
+        _gameBoard = _spawnableEnv.GetComponentInChildren<GameBoard>();
+        // _mocapController = _spawnableEnv.GetComponentInChildren<MocapController>();
+
+        // to do, error check the behavior paramaters
+        
+        _gameBoard.InitializeBoard(Size);
+    }
+
+    override public void CollectObservations(VectorSensor sensor)
+    {
+        
+    }
+
+    override public void CollectDiscreteActionMasks(DiscreteActionMasker actionMasker)
+    {
+        
+    }
+
+    override public void OnActionReceived(float[] vectorAction) 
+    {
+        
+    }
+
+    override public void OnEpisodeBegin()
+    {
+        
+    }
+
+
+
+}
