a0ab4c0f1fa784c094d472be23359f8a11ddf346
Joe Booth <joe@joebooth.com>
2020-06-22 18:57:48 -0700

    feature: board gives max in a row

--- TicTacAlpha/Assets/Scripts/GameBoard.cs
+++ TicTacAlpha/Assets/Scripts/GameBoard.cs
@@ -11,6 +11,16 @@ public class GameBoard : MonoBehaviour
     List<Cell> _cells;
     public int Size;
 
+    public int BestScoreTeamId1;
+    public int BestScoreTeamId2;
+    public int LastRow;
+    public int LastColumn;
+    public int LastTeamId;
+    public List<int> LastHorizontal;
+    public List<int> LastVertical;
+    public List<int> LastLeftToRight;
+    public List<int> LastRightToLeft;
+
     bool _hasInitializedBoard;
     int _nextPlayerId;
     // Start is called before the first frame update
@@ -63,6 +73,8 @@ public class GameBoard : MonoBehaviour
             cells.TeamId = 0;
         }
         _nextPlayerId = 1;
+        BestScoreTeamId1=0;
+        BestScoreTeamId2=0;
     }
 
     public void CollectObservationsForPlayer(VectorSensor sensor, int playerId)
@@ -107,14 +119,6 @@ public class GameBoard : MonoBehaviour
         _nextPlayerId = playerId == 1 ? 2 : 1;
         UpdateScore(action);
     }
-
-    public List<int> LastHorizontal;
-    public List<int> LastVertical;
-    public List<int> LastLeftToRight;
-    public List<int> LastRightToLeft;
-    public int LastRow;
-    public int LastColumn;
-    public int LastTeamId;
 
     void UpdateScore(int action)
     {
@@ -172,7 +176,36 @@ public class GameBoard : MonoBehaviour
             cell = _cells[idx];
             LastRightToLeft.Add(cell.TeamId);
         }
-
+        string lastHorizontalStr = string.Join("", LastHorizontal);
+        string lastVerticalStr = string.Join("", LastVertical);
+        string lastLeftToRightStr = string.Join("", LastLeftToRight);
+        string lastRightToLeftStr = string.Join("", LastRightToLeft);
+        foreach (var i in Enumerable.Range(0,Size))
+        {
+            var score = Size-i;
+            if (score > BestScoreTeamId1)
+            {
+                var target = string.Join("", Enumerable.Range(0,score).Select(x=>1));
+                bool newHighScore = 
+                    lastHorizontalStr.Contains(target)
+                    | lastVerticalStr.Contains(target)
+                    | lastLeftToRightStr.Contains(target)
+                    | lastRightToLeftStr.Contains(target);
+                if (newHighScore)
+                    BestScoreTeamId1 = score;
+            }
+            if (score > BestScoreTeamId2)
+            {
+                var target = string.Join("", Enumerable.Range(0,score).Select(x=>2));
+                bool newHighScore = 
+                    lastHorizontalStr.Contains(target)
+                    | lastVerticalStr.Contains(target)
+                    | lastLeftToRightStr.Contains(target)
+                    | lastRightToLeftStr.Contains(target);
+                if (newHighScore)
+                    BestScoreTeamId2 = score;
+            }
+        }
     }
 
     public bool HasEnded()
